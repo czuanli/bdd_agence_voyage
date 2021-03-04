302904b0a8fe1e58968c1314b3de7d607b74ff05
Zerui-WANG <71635569+Zerui-WANG@users.noreply.github.com>
2021-03-04 12:29:54 +0100

    update conception schema

--- project1.sql
+++ project1.sql
@@ -1,75 +1,89 @@
 drop database layxoTravelAgency;
 create database layxoTravelAgency;
 use layxoTravelAgency;
-create table Person(
-	idPerson int unsigned primary key auto_increment,
+create table Customer(
+	idCustomer int unsigned auto_increment,
+    addressCustomer varchar(64) not null,
     lastName varchar(64) not null,
     firstName varchar(64) not null,
     email varchar(64) not null,
     pwd varchar(64) not null,
-    phone varchar(64) not null
-);
-create table Customer(
-	idCustomer int references Person,
-    primary key(idCustomer),
-    addressCustomer varchar(64) not null
+    phone varchar(64) not null,
+    primary key(idCustomer)
 );
 create table Staff(
-	idStaff int references Person,
-    primary key(idStaff),
+	idStaff int unsigned auto_increment,
     salary float not null,
-    hiringDate date
+    hiringDate date,
+    lastName varchar(64) not null,
+    firstName varchar(64) not null,
+    email varchar(64) not null,
+    pwd varchar(64) not null,
+    phone varchar(64) not null,
+    primary key(idStaff)
 );
 create table Booking(
-	idBooking int unsigned primary key auto_increment,
+	idBooking int unsigned auto_increment,
     createdAt timestamp default current_timestamp,
     totalFee float not null,
-	idStaff int,
+	idStaff int unsigned,
+    primary key(idBooking),
     foreign key(idStaff)
 		references Staff(idStaff),
-	idCustomer int,
+	idCustomer int unsigned,
     foreign key(idCustomer)
 		references Customer(idCustomer)
 );
-create table Service(
-	idService int unsigned primary key auto_increment,
-    dateStart date,
-    dateEnd date,
-    fee float not null
-);
 create table Attraction(
-	idAttraction int references Service,
-    primary key(idAttraction),
+	idAttraction int unsigned auto_increment,
     dateStart date,
     dateEnd date,
     fee float not null,
     city varchar(64) not null,
     country varchar(64) not null,
     weather varchar(64) not null,
-    travelRoute varchar(255) not null
+    travelRoute varchar(255) not null,
+    primary key(idAttraction)
 );
 create table Room(
-	idRoom int references Service,
-    primary key(idRoom),
+	idRoom int unsigned auto_increment,
     dateStart date,
     dateEnd date,
     fee float not null,
-    roomNumber int,
-    roomName varchar(64) not null,
+    roomNumber int not null,
+    roomName varchar(64),
     isReserved tinyint(1) not null,
     isReady tinyint(1) not null,
-    roomType varchar(64) not null
+    roomType varchar(64) not null,
+    primary key(idRoom)
+);
+create table BookingAttraction(
+	idBooking int unsigned,
+	foreign key(idBooking)
+		references Booking(idBooking),
+	idAttraction int unsigned,
+    foreign key(idAttraction)
+		references Attraction(idAttraction)
+);
+create table BookingRoom(
+	idBooking int unsigned,
+	foreign key(idBooking)
+		references Booking(idBooking),
+	idRoom int unsigned,
+    foreign key(idRoom)
+		references Room(idRoom)
 );
 create table Hotel(
-	idHotel int unsigned primary key auto_increment,
+	idHotel int unsigned auto_increment,
 	hotelName varchar(64) not null,
     addressHotel varchar(64) not null,
     zipCode varchar(64) not null,
     cityHotel varchar(64) not null,
     countryHotel varchar(64) not null,
     phone varchar(64) not null,
-    email varchar(64) not null
+    email varchar(64) not null,
+    primary key(idHotel)
 );
 use layxoTravelAgency;
 show tables;
-describe room;
+describe BookingRoom;
